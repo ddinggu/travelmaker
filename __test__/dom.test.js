eaac9e2a04f0653beaa2b7a0f32bcd64ee0d7165
ddinggu <travelrudwls@gmail.com>
2019-04-04 00:38:32 +0900

    add travis ci and test with yarn pm

--- __test__/dom.test.js
+++ __test__/dom.test.js
@@ -1,2 +1,15 @@
+'use strict';
+
 const $ = require('jquery');
-const html = require('fs').readFileSync('');
+const path = require('path');
+const mockDir = path.dirname(__dirname + '/../googleMapMarkerLabels.html');
+const html = require('fs').readFileSync(mockDir);
+
+describe('Google Map', () => {
+	document.documentElement.innerHTML = html;
+	test('render successfully', () => {
+		const containerBox = $('#map');
+		expect(containerBox.has('div')).toBeTruthy();
+	});
+});
+
